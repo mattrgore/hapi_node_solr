27e33cd4f719b7173d1462b046a57449bc65629f
Matt Gore <mgore@enservio.com>
2016-11-05 18:21:02 -0400

    fixed unit tests

--- test/server.js
+++ test/server.js
@@ -25,45 +25,4 @@ describe('Example Server', () => {
             done();
         })
     });
-
-    describe('/products endpoints', () => {
-        describe('GET /products', () => {
-            it('handle naked request', (done) => {
-                server.inject('/products', (res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.result[0]).to.deep.equal({id: 1, name: 'Guitar'});
-                    expect(res.result[1]).to.deep.equal({id: 2, name: 'Banjo'});
-                    done();
-                })
-            });
-
-            it('handle request with param name', (done) => {
-                server.inject('/products?name=Guitar', (res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.result[0]).to.deep.equal({id: 1, name: 'Guitar'});
-                    expect(res.result).to.have.length(1);
-                    done();
-                })
-            });
-
-
-        });
-
-        function testFor(id, value, callback) {
-            server.inject('/products/' + id, (res) => {
-                expect(res.statusCode).to.equal(200);
-                expect(res.result).to.deep.equal(value);
-                callback();
-            })
-        }
-
-        it('GET /products/x', (done) => {
-            testFor(1, {id: 1, name: 'Guitar'}, () => {
-                testFor(2, {id: 2, name: 'Banjo'}, done)
-            })
-        });
-
-    });
-
-
 });
